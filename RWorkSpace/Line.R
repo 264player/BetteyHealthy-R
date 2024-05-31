2d6a03cc202b0cb8a15db04e4afe813a8280723c
hollow_264 <2309707428@qq.com>
2024-05-31 17:24:48 +0800

    第一次多元线性回归预测，0分

--- RWorkSpace/Line.R
+++ RWorkSpace/Line.R
@@ -1,6 +1,6 @@
 library(ggplot2)
 library(gridExtra)
-source <- read.csv("D:\\studyDay\\RWorkSpace\\data\\battery\\train_data.csv")
+source <- read.csv("D:\\studyDay\\RWorkSpace\\data\\battery\\train_data.csv")[,c("cycle","capacity","SoH","resistance","CCCT","CVCT")]
 str(source)
 summary(source)
 
--- RWorkSpace/testModel.R
+++ RWorkSpace/testModel.R
@@ -1,5 +1,12 @@
 model <- readRDS("my_linear_model1.rds")
 
-testData <- read.csv("D:\\studyDay\\RWorkSpace\\data\\battery\\test_data.csv")
+Data <- read.csv("D:\\studyDay\\RWorkSpace\\data\\battery\\test_data.csv")
+testData <- subset(Data,SoH == -100)
 str(testData)
 summary(testData)
+
+result <- predict(model,testData[,c("cycle","capacity","resistance","CCCT","CVCT")])
+
+output_values <- cbind(testData[,c("cycle","CS_Name")],result)
+
+write.csv(output_values,"submission.csv",row.names = TRUE)
