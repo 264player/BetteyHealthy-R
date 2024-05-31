936639f556da8f1a7633c1f0c35c6bf5680f34fb
hollow_264 <2309707428@qq.com>
2024-05-31 19:20:27 +0800

    修正输出格式，0.53

--- RWorkSpace/testModel.R
+++ RWorkSpace/testModel.R
@@ -1,12 +1,13 @@
 model <- readRDS("my_linear_model1.rds")
 
-Data <- read.csv("D:\\studyDay\\RWorkSpace\\data\\battery\\test_data.csv")
-testData <- subset(Data,SoH == -100)
+sourceData <- read.csv("D:\\studyDay\\RWorkSpace\\data\\battery\\test_data.csv")
+str(sourceData)
+testData <- subset(sourceData,SoH == -100)
 str(testData)
 summary(testData)
 
 result <- predict(model,testData[,c("cycle","capacity","resistance","CCCT","CVCT")])
 
-output_values <- cbind(testData[,c("cycle","CS_Name")],result)
+output_values <- cbind(testData[,c("X","cycle","CS_Name")],result)
 
-write.csv(output_values,"submission.csv",row.names = TRUE)
+write.csv(output_values,"submission.csv",row.names = FALSE)
